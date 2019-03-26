81f72477a10cc2ca03c1a658ff1932cddcc1a4a2
ezetina86 <jenzetin@gmail.com>
2019-03-26 16:19:49 -0600

    Counting Tries
    Now the game tells you the number  of tries you made to guess the number

--- src/HiLo.java
+++ src/HiLo.java
@@ -18,28 +18,35 @@ public class HiLo {
 			 * only number between 0 and 0.99999, so I multiply theNumber * 100
 			 * 
 			 */
-			//Expanding My Range
-			//int theNumber = (int) (Math.random() * 100 + 1);
+			// Expanding My Range
+			// int theNumber = (int) (Math.random() * 100 + 1);
 			int theNumber = (int) (Math.random() * 200 - 100);
 			// System.out.println(theNumber);
 
 			int guess = 0;
+			int numberOfTries = 0;
+
 			while (guess != theNumber) {
 				System.out.println("Guess a number between -100 and 100:");
 				guess = scanner.nextInt();
 				System.out.println("You entered " + guess + ".");
-				if (guess < theNumber)
+				if (guess < theNumber) {
 					System.out.println(guess + " is too low. Try again.");
-				else if (guess > theNumber)
-					//I found an error here I was writing an extra word 'low'
+					numberOfTries++;
+				} else if (guess > theNumber) {
+					// I found an error here I was writing an extra word 'low'
 					System.out.println(guess + " is high. Try again.");
-					//System.out.println(guess + " is high low. Try again.");
-				else
-					System.out.println(guess + " is correct.");
+					// System.out.println(guess + " is high low. Try again.");
+					numberOfTries++;
+				} else {
+					System.out.println(guess + " is correct. You win!");
+					System.out.println("it only took you "+ numberOfTries + " tries! Good work!");
+				}
+
 			}
 		} while (playAgain.equalsIgnoreCase("y"));
 		System.out.println("Thank you for playing! Goodbye.");
-		//just to  avoid fill up memory
+		// just to avoid fill up memory
 		scanner.close();
 	}
 }
